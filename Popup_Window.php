a70b2bbfecf6e3b7b7c8a4bf25429a523b0dc974
Jzamae <jiezamaeapid@gmail.com>
2016-10-29 02:28:52 +0800

    add Popup_Window and Status_Bar files

---
+++ Popup_Window.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en">
+<head>
+</head>
+<body>
+<script type="text/javascript">
+// Popup window code
+function newPopup(url) {
+	popupWindow = window.open(
+		url,'popUpWindow','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
+}
+</script>
+<p>Popup Window:</p>
+<p><a href="JavaScript:newPopup('http://www.quackit.com/html/codes/');">Get your HTML codes in a popup!</a></p>
+<div class="more-info">
+<p>More info: <a href="/javascript/popup_windows.cfm">Popup Windows</a></p>
+</div>
+</body>
+</html>
