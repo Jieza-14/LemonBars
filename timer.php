6a69fb9ee11126199172d8cf26cc4bfb017e8b99
Jzamae <jiezamaeapid@gmail.com>
2016-10-29 02:19:01 +0800

    adding timer

---
+++ timer.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+</head>
+<body>
+<p>Timed Refresh:</p>
+<script type="text/JavaScript">
+<!--
+function timedRefresh(timeoutPeriod) {
+	setTimeout("location.reload(true);",timeoutPeriod);
+}
+//   -->
+</script>
+<p><a href="javascript:timedRefresh(3000)">Refresh this page in 3 seconds...</a></p>
+</div>
+<div class="more-info">
+<p>More info: <a href="/javascript/javascript_refresh_page.cfm">JavaScript Refresh</a></p>
+</div>
+</body>
+</html>
